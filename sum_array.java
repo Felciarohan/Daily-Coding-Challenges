b9b64e3b775f4d23711c6c6a944d234a76aa57ff
Felcia A <jeyakumararul91@gmail.com>
2026-03-11 22:20:19 +0530

    Add files via upload

---
+++ sum_array.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class sum_array {
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+        int n=s.nextInt();
+        int a[]=new int[n];
+        for(int i=0;i<n;i++){
+            a[i]=s.nextInt();
+        }
+        int sum=0;
+        for(int i=0;i<n;i++){
+            sum+=a[i];
+        }
+        System.out.print(sum);
+    }
+}

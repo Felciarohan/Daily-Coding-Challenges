71e91b57b59eb531b5c835eab22e3e86223ab6e7
Felcia A <jeyakumararul91@gmail.com>
2026-03-15 14:25:48 +0530

    Add files via upload

---
+++ Sum_of_digits.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Sum_of_digits {
+    public static void main(String[]args){
+        Scanner s=new Scanner(System.in);
+        int n=s.nextInt();
+        if(n%9==0)
+        System.out.println("9");
+        else
+        System.out.println(n%9);
+    }
+}
